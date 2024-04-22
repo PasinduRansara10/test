1d9c8da76ba86894d6fb7ae573ad6ac199bcb5ec
Pasindu Ransara <140839432+PasinduRansara10@users.noreply.github.com>
2024-04-22 11:39:40 +0530

    login file
    it is login page for users.

---
+++ Untitled1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age;  //variable creation
+        printf("Type a your age:"); //ask the question
+        scanf("%d", &age);    //%d - integer = numbers
+        printf("\nYour age is:     %d", age);
+    return 0;
+
+}
